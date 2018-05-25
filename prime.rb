d2641d32aecaa485a141f8379894bddd66f7532d
Paul Bomba <paul@paulbomba.com>
2018-05-25 05:19:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(number)
+	if number <= 0
+		return FALSE
+	elsif number == 1
+		return FALSE
+	else
+		i = 2
+		while i < number do
+			if number % i == 0
+				break
+			else
+				i += 1
+				break if i == number
+		  end
+	end
+	end
+		if i == number
+			return TRUE
+		else
+			return FALSE
+		end
+end
