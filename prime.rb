83d947e369a2c8cec86e6e7442d323374ea1f910
Paul Bomba <paul@paulbomba.com>
2018-05-25 05:13:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(number)
+	if number <= 0
+		return false
+	elsif number == 1
+		return false
+#	elsif number == 2
+#		return true
+	else
+		i = 2
+		while i < number do
+			if number % i == 0
+				break
+			else
+				i += 1
+				break if i == number
+		end
+		if i == number
+			return true
+		else
+			return false
+		end
+	end
+end
